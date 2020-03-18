4d681eedd734e439fe28e5833c3154298a250d51
hago86 <41424550+hago86@users.noreply.github.com>
2020-03-18 20:02:37 +0000

    Create main.cpp

---
+++ Lesson_04/MissingInteger/main.cpp
@@ -0,0 +1,24 @@
+/*
+Note that the input array can contain duplicates and
+negative numbers.
+*/
+
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    
+    set <int> s1;
+    for(auto& i1: A)
+        s1.insert(i1);
+    
+    int i, length;
+    length = A.size() + 1;
+
+    for(i = 1; i <= length; i++)
+    {
+        if (s1.find(i) == s1.end())
+            return i;
+    }
+    
+    return 1;
+}
